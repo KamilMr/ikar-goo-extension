87227083dbcccc464384857c77a09b2185b9c8a0
Kamil Mrowka <kamil@chicos.life>
2023-03-04 10:20:52 +0100

    Init

---
+++ index.js
@@ -0,0 +1,8 @@
+// get root
+const root = document.getElementById('root');
+
+// get current document html
+const html = document.documentElement.innerHTML;
+
+// inject html into root
+root.innerHTML = html;
